ccbbd7a9cd9997c2b3980597542f101950304a02
igreatdev <izudongreat@gmail.com>
2019-02-06 11:52:54 +0100

    Added Cors middleware

--- src/Application.php
+++ src/Application.php
@@ -54,6 +54,9 @@ class Application extends BaseApplication
         if (Configure::read('debug')) {
             $this->addPlugin(\DebugKit\Plugin::class);
         }
+
+        // Add Cors plugin
+        $this->addPlugin('Cors');
     }
 
     /**
